d73e493ce2c50137e9040c50e9b54e9c15439280
Rajkishor Pradhan <rajkp7598@gmail.com>
2021-03-19 21:00:19 +0530

    Reading data from MongoDB

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
-const MongoClient = require("mongodb").MongoClient;
-const assert = require("assert");
+const mongoose = require("mongoose");
 
-// Connection URL
-const url = "mongodb://localhost:27017";
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Database Name
-const dbName = "Fruits";
-
-// Create a new MongoClient
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Use connect method to connect to the Server
-client.connect(function (err) {
-    assert.strictEqual(null, err);
-    console.log("Connected successfully to server");
-
-    const db = client.db(dbName);
-
-    client.close();
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
 });
 
-const insertDocuments = function (db, callback) {
-    // Get the documents collection
-    const collection = db.collection("Fruits");
-    // Insert some documents
-    collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }], function (err, result) {
-        assert.strictEqual(err, null);
-        assert.strictEqual(3, result.result.n);
-        assert.strictEqual(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    });
-};
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name: "Rohit",
+    age: 25
+});
+const person1 = new Person({
+    name: "Kunal",
+    age: 27
+});
+const person2 = new Person({
+    name: "Chacha",
+    age: 91
+});
+
+// person.save();
+// person1.save();
+// person2.save();
+
+// Person.insertMany([person, person1, person2], function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Success");
+//     }
+// });
+
+Person.find(function (err, myFruits) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(myFruits);
+    }
+});
